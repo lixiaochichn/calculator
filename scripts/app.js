442c18bf6528cd3c68a0e0caa5f5c4f189a548a2
lixiaochichn@gmail.com <lixiaochichn@gmail.com>
2017-07-24 23:46:33 +0800

    already002

--- scripts/app.js
+++ scripts/app.js
@@ -6,36 +6,120 @@
 
 
   $number = document.querySelectorAll('.number');
-  // $plus = document.querySelector('.arithmetic.plus');
-  // $sub = document.querySelector('.arithmetic.sub');
-  // $noun = document.querySelector('.arithmetic.noun');
-  // $division = document.querySelector('.arithmetic.division');
-  // $equal = document.querySelector('.equal');
-  // $clear = document.querySelector('.num.clear');
+  $plus = document.querySelector('.arithmetic.plus');
+  $sub = document.querySelector('.arithmetic.sub');
+  $noun = document.querySelector('.arithmetic.noun');
+  $division = document.querySelector('.arithmetic.division');
+  $equal = document.querySelector('.equal');
+  $dot = document.querySelector('.dot');
+  $clear = document.querySelector('.num.clear');
+  $header = document.querySelector('.header');
 
   function setresult(value) {
-    $result.innerText = String(value);
-  }
+    expr = String(value);
+    $result.innerText = expr;
+  };
 
   function setformula(value) {
-    $formula.innerText = String(value);
-  }
-let expr = 0;
-  setresult('0');
-  setformula('玩命运算中...');
+    exprf = String(value);
+    $formula.innerText = exprf;
+  };
+  let expr = '0';
+  let exprf = '';
+  let ready = 0;
+  let operate = ['+', '-', '*', '/', '.'];
+  setresult(expr);
+  setformula(exprf);
 
-for(let i=0; i<$number.length; i++) {
-$number[i].addEventListener('click', function(event){
-  let number = event.currentTarget.getAttribute('data-number');
-  if(expr !== 0) {
-    expr = expr + number;
-  } else {
-    expr = number;
-  }
+  for (let i = 0; i < $number.length; i++) {
+    $number[i].addEventListener('click', function (event) {
+      if (ready === 1) {
+        expr = "0";
+        let exprf = '';
+        setresult(expr);
+        setformula(exprf);
+        ready = 0;
+      };
+      let number = event.currentTarget.getAttribute('data-number');
+      if (expr !== '0') {
+        expr = String(expr + number);
+      } else {
+        expr = String(number);
+      }
+      setresult(expr);
+    });
+  };
+
+  $plus.addEventListener('click', function () {
+    if (operate.indexOf(expr[expr.length - 1]) > -1) {
+      expr = expr.slice(0, expr.length - 1) + '+';
+    } else { expr = expr + '+'; }
     setresult(expr);
-});
+    ready = 0;
+  });
 
-}
+  $sub.addEventListener('click', function () {
+    if (operate.indexOf(expr[expr.length - 1]) > -1) {
+      expr = expr.slice(0, expr.length - 1) + '-';
+    } else { expr = expr + '-'; }
+    setresult(expr);
+    ready = 0;
+  });
+
+  $noun.addEventListener('click', function () {
+    if (operate.indexOf(expr[expr.length - 1]) > -1) {
+      expr = expr.slice(0, expr.length - 1) + '*';
+    } else { expr = expr + '*'; }
+    setresult(expr);
+    ready = 0;
+  });
+  $division.addEventListener('click', function () {
+    if (operate.indexOf(expr[expr.length - 1]) > -1) {
+      expr = expr.slice(0, expr.length - 1) + '/';
+    } else { expr = expr + '/'; }
+    setresult(expr);
+    ready = 0;
+  });
+
+  $dot.addEventListener('click', function () {
+    if (ready === 1) {
+      expr = "0";
+      let exprf = '';
+      setresult(expr);
+      setformula(exprf);
+      ready = 0;
+    };
+    if (expr[expr.length - 1] === '.') { }
+    else if (operate.indexOf(expr[expr.length - 1]) > -1) {
+      expr = expr + '0.';
+    } else { expr = expr + '.'; }
+    setresult(expr);
+  });
+  $clear.addEventListener('click', function () {
+    expr = "0";
+    let exprf = '';
+    setresult(expr);
+    setformula(exprf);
+  });
+  $equal.addEventListener('click', function () {
+    if (operate.indexOf(expr[expr.length - 1]) > -1) {
+      if (ready === 0) exprf = expr.slice(0, expr.length - 1) + '=';
+      expr = eval(expr.slice(0, expr.length - 1));
+    } else {
+      if (ready === 0) exprf = expr + '=';
+      expr = eval(expr);
+    }
+    if (String(eval(expr) - eval(expr).toFixed(0)).length > 8) expr = eval(expr).toFixed(6);
+  
+    setresult(expr);
+    setformula(exprf);
+    ready = 1;
+  });
+
+  function resize() {
+    let maxWidth = $resullt.clientWidth;
+    if($resullt.scrollWidth > maxWidth){$resullt.style.tranform = 'scale(${maxWidth / $resullt.scrollWidth})'};
+  }
 
 
 })();
